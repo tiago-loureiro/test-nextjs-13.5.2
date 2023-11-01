35e4834c49bf96799e0782605401817d04f5a706
Tiago Loureiro <tiago.loureiro@gmail.com>
2023-11-01 19:52:25 +0100

    Minimal

--- app/cars.js
+++
@@ -1,7 +0,0 @@
-import { createCar } from '../../lib/redis';
-
-export default async function handler(req, res) {
-    console.log(`Handling`);
-    const id = await createCar(req.body);
-    res.status(200).json({ id })
-}
