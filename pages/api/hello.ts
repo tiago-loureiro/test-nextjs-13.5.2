f07568f2007fe4b932472c6af027d47831ec6044
Tiago Loureiro <tiago.loureiro@gmail.com>
2023-11-08 12:51:58 +0100

    Simple againz

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,5 +2,6 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function test(req: NextApiRequest, res: NextApiResponse) {
     await new Promise(r => setTimeout(r, 5000));
+    res.setHeader('x-vercel-dummy', "xxxxxxxxxxx".repeat(20000));
     res.status(200).json('')
 }
