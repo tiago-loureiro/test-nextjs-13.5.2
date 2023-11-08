24fb970b94b7be81fa9f8b254ef36681a5dfc987
Tiago Loureiro <tiago.loureiro@gmail.com>
2023-11-08 13:04:44 +0100

    Bigger

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function test(req: NextApiRequest, res: NextApiResponse) {
-    const headerSize = 100000
+    const headerSize = 48000
     res.setHeader('x-vercel-dummy', "x".repeat(headerSize));
     res.status(200).json({"size": headerSize});
 }
