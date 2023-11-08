091b7ca6cbb352abe971fae19bc44d9806d3a383
Tiago Loureiro <tiago.loureiro@gmail.com>
2023-11-08 12:25:41 +0100

    Simple again, 9 secs

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function terms(req: NextApiRequest, res: NextApiResponse) {
-    await new Promise(r => setTimeout(r, 9999));
+    await new Promise(r => setTimeout(r, 9900));
     res.status(200).json('')
 }
