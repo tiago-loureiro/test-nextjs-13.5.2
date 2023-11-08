82355b3d2807c29ae138bd61f93091334a219c8f
Tiago Loureiro <tiago.loureiro@gmail.com>
2023-11-08 12:59:57 +0100

    64kB header

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function test(req: NextApiRequest, res: NextApiResponse) {
-    await new Promise(r => setTimeout(r, 5000));
-    res.setHeader('x-vercel-dummy', "xxxxxxxxxxx".repeat(20000));
+    await new Promise(r => setTimeout(r, 1000));
+    res.setHeader('x-vercel-dummy', "x".repeat(64000));
     res.status(200).json('')
 }
