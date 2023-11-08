e2d99a523ad7cba36d714af7645bafb1b8d8c4e5
Tiago Loureiro <tiago.loureiro@gmail.com>
2023-11-08 12:47:11 +0100

    Simple againz

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
-export default async function terms(req: NextApiRequest, res: NextApiResponse) {
-    await new Promise(r => setTimeout(r, 9900));
+export default async function test(req: NextApiRequest, res: NextApiResponse) {
+    await new Promise(r => setTimeout(r, 5000));
     res.status(200).json('')
 }
