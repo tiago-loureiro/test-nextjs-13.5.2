9be5c15bc098fa098d4d32b6f18e66d2ba334ed9
Tiago Loureiro <tiago.loureiro@gmail.com>
2023-11-09 15:32:37 +0100

    Param

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,9 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function test(req: NextApiRequest, res: NextApiResponse) {
-    const hz = req.query.q || '';
+    const hz = req.query.q || '1';
     console.log(hz);
-    const headerSize = parseInt(hz);
+    const headerSize = parseInt(Array.isArray(hz) ? hz[0] : hz);
     console.log("Header size: " + headerSize);
     res.setHeader('x-vercel-dummy', "x".repeat(headerSize));
     res.status(200).json({"size": headerSize});
