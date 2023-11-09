4f35a2164aba46f5d944daf211b227c4355c9471
Tiago Loureiro <tiago.loureiro@gmail.com>
2023-11-09 15:56:02 +0100

    Missing end

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,9 +2,8 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function test(req: NextApiRequest, res: NextApiResponse) {
     const hz = req.query.q || '1';
-    console.log(hz);
     const headerSize = parseInt(Array.isArray(hz) ? hz[0] : hz);
-    console.log("Header size: " + headerSize);
     res.setHeader('x-vercel-dummy', "x".repeat(headerSize));
     res.status(200).json({"size": headerSize});
+    res.end();
 }
