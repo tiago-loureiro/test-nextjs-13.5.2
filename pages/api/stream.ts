1e483369c7341744ba21e3c7749d3aa54e732cf5
Tiago Loureiro <tiago.loureiro@gmail.com>
2023-11-15 10:53:46 +0100

    Try it

--- pages/api/stream.ts
+++ pages/api/stream.ts
@@ -2,19 +2,22 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { PassThrough } from 'stream';
 
 export default async function stream(req: NextApiRequest, res: NextApiResponse) {
-    const hz = req.query.q || '1';
-    const headerSize = parseInt(Array.isArray(hz) ? hz[0] : hz);
+    const sz = req.query.q || '1';
+    const count = Math.max(parseInt(Array.isArray(sz) ? sz[0] : sz), 500);
+
     const myStream = new PassThrough();  
     // Write data to the stream
     myStream.write('Hello, world!');
     // Pipe the stream to the response
-    res.setHeader('x-vercel-dummy', "x".repeat(headerSize));
-    let iterCount = 100;
+    res.setHeader('x-vercel-dummy', "x".repeat(count));
+
+    let iterCount = count;
     while (iterCount > 0) {
         console.log(`Fetches left ${iterCount}`);
         await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
         iterCount--;
     }
+
     myStream.pipe(res);    
     myStream.end();
 };
