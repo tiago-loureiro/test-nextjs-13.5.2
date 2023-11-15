83cd3e8c93c74022c653b337b52f67bacb3b8427
Tiago Loureiro <tiago.loureiro@gmail.com>
2023-11-15 23:50:13 +0100

    Stream

--- pages/api/simple.ts
+++ pages/api/simple.ts
@@ -1,13 +1,22 @@
-import { count } from 'console';
-import { NextApiRequest, NextApiResponse } from 'next'
-import { PassThrough } from 'stream';
-
-export default async function simple(req: NextApiRequest, res: NextApiResponse) {
-    const myStream = new PassThrough();  
-    // Write data to the stream
-    myStream.write('Hello, world!');
-    res.setHeader('x-vercel-dummy', "xello xeader");
-
-    myStream.pipe(res);    
-    myStream.end();
-};
+// Can be 'nodejs', but Vercel recommends using 'edge'
+export const runtime = 'edge';
+// Prevents this route's response from being cached
+export const dynamic = 'force-dynamic';
+ 
+export default async function GET() {
+  // This encoder will stream your text
+  const encoder = new TextEncoder();
+  const customReadable = new ReadableStream({
+    start(controller) {
+      // Start encoding 'Basic Streaming Test',
+      // and add the resulting stream to the queue
+      controller.enqueue(encoder.encode('Basic Streaming Test'));
+      // Prevent anything else being added to the stream
+      controller.close();
+    },
+  });
+ 
+  return new Response(customReadable, {
+    headers: { 'Content-Type': 'text/html; charset=utf-8' },
+  });
+}
