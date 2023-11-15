a0dacc234652cc0018f03d4232374429a36b8921
Tiago Loureiro <tiago.loureiro@gmail.com>
2023-11-15 13:01:14 +0100

    A bit more debug

--- pages/api/stream.ts
+++ pages/api/stream.ts
@@ -11,13 +11,17 @@ export default async function stream(req: NextApiRequest, res: NextApiResponse) 
     // Pipe the stream to the response
     res.setHeader('x-vercel-dummy', "x".repeat(count));
 
-    let iterCount = count;
-    while (iterCount > 0) {
-        console.log(`Fetches left ${iterCount}`);
-        await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
-        iterCount--;
-    }
+    const start = new Date().getTime();
+    console.log(`Starting at ${start}`);
 
+    await Promise.all(
+        [...Array(count).keys()].map(async (element) => {
+            console.log(`Req nr ${element} @ ${new Date().getTime()}`);
+            return fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+        })
+    )
+
+    console.log(`Ended ${(new Date().getTime()) - start} ms later`);
     myStream.pipe(res);    
     myStream.end();
 };
